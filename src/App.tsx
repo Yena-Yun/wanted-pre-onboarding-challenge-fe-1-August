bc0a81a8bb4d5e112b4a7a5a504934750b5a0baa
Yena-Yun <cheryleduart@gmail.com>
2022-08-05 13:31:09 +0900

    :spakles: 기본적인 Route 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { HomePage, LoginPage, TodoListPage } from './components';
+
 function App() {
-  return <div className='App'>냥냥</div>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+          <li>
+            <Link to='/todolist'>TodoList</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/todolist' element={<TodoListPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import { HomePage } from './Home.page';
+import { LoginPage } from './Login.page';
+import { TodoListPage } from './TodoList.page';
+
+export { HomePage, LoginPage, TodoListPage };
