1fe1f082632ddc66cf0d6ffdc6e303771dbdc316
Yena-Yun <cheryleduart@gmail.com>
2022-08-05 13:38:15 +0900

    :sparkles: json-server로 데이터 받아와서 렌더링하기

--- src/components/Login.page.js
+++ src/components/Login.page.js
@@ -1,3 +1,36 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/data').then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>LoginPage</h2>
+      {data.map((todo) => {
+        return (
+          <ul key={todo.id}>
+            <li>
+              <h3>{todo.title}</h3>
+              <p>{todo.content}</p>
+              <span>{todo.createdAt}</span>
+              <span>{todo.updatedAt}</span>
+            </li>
+          </ul>
+        );
+      })}
+    </>
+  );
 };
