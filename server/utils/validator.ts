a1736007c238006d8bc937ae0b39f781df77b2a4
Yena-Yun <cheryleduart@gmail.com>
2022-08-17 13:43:53 +0900

    :wrench: login/User 관련 validator 추가

--- server/utils/validator.ts
+++ server/utils/validator.ts
@@ -1,6 +1,45 @@
 import validator from 'validator';
 
+export const loginValidator = (loginForm: {
+  email: string;
+  password: string;
+}) => {
+  if (
+    validator.isEmpty(loginForm.email) ||
+    validator.isEmpty(loginForm.password)
+  ) {
+    return {
+      isValid: false,
+      message: USER_VALIDATION.EMPTY_INPUT,
+    };
+  }
+
+  if (!validator.isEmail(loginForm.email)) {
+    return {
+      isValid: false,
+      message: USER_VALIDATION.INVALID_EMAIL,
+    };
+  }
+
+  if (!validator.isLength(loginForm.password, { min: 8 })) {
+    return {
+      isValid: false,
+      message: USER_VALIDATION.INVALID_PASSWORD,
+    };
+  }
+
+  return { isValid: true };
+};
+
 export const TODO_VALIDATION = {
   NOT_FOUND: 'todo를 찾는 도중 문제가 발생했습니다.',
   INVALID_VALUE: '입력란을 다시 확인해주세요.',
+};
+
+export const USER_VALIDATION = {
+  EMPTY_INPUT: '이메일/비밀번호 값이 비어있습니다.',
+  INVALID_EMAIL: '이메일 형식에 맞게 입력해주세요.',
+  INVALID_PASSWORD: '비밀번호 길이는 8 이상이어야 합니다.',
+  NOT_FOUND_USER: '유저가 없어 로그인에 실패했습니다.',
+  EXIST_USER: '이미 존재하는 유저입니다.',
 };
